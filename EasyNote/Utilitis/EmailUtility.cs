5c9a7b71e46a413b2d994cb5f1ba3467d7b6c436
mmustafaseker <mustiseker.06@gmail.com>
2021-03-29 15:54:09 +0300

    EmailServicesAdded

---
+++ EasyNote/Utilitis/EmailUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EasyNote.Utilitis
+{
+    public static class EmailUtility
+    {
+        public static async Task SendEmailAsync(string to, string subject, string body)
+        {
+            using (var smtp= new SmtpClient("mustafaseker.net"))
+            {
+                smtp.Credentials = new NetworkCredential("easynote@mustafaseker.net", "********");
+                //smtp.EnableSsl = true;
+                smtp.Port = 587;
+                MailMessage message = new MailMessage("easynote@mustafaseker.net",to, subject, body);
+                message.IsBodyHtml = true;
+                await smtp.SendMailAsync(message);
+            }
+        }
+    }
+}
